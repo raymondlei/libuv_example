057a20811761ae5109fca0a4f39a24dd70639d40
Raymond Lei <riemann.lei@gmail.com>
2017-05-29 03:14:19 -0500

    added example

---
+++ src/ex01.c
@@ -0,0 +1,29 @@
+/*
+ * ex01.c
+ *
+ *  Created on: May 29, 2017
+ *      Author: u904
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <uv.h>
+#include <uv-unix.h>
+
+static uv_thread_t thread;
+
+
+static void thread_cb(void* arg) {
+  printf("hello thread!\n");
+}
+
+
+int main() {
+  int r = uv_thread_create(&thread, thread_cb, NULL);
+  assert(r == 0);
+
+  /* pause execution of this thread until the spawned thread has had
+   * time to finish execution. */
+  uv_thread_join(&thread);
+
+  return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,23 +1,34 @@
-#include <assert.h>
 #include <stdio.h>
 #include <uv.h>
-#include <uv-unix.h>
 
-static uv_thread_t thread;
+//gcc -pthread ../deps/libuv/bin/*.o ex2.c -lrt -ldl -lm -o ex2
 
+uv_loop_t* loop;
 
-static void thread_cb(void* arg) {
-  printf("hello thread!\n");
+void timer_cb1 (uv_timer_t* timer, int status) {
+	printf("1\n", status);
+	uv_timer_stop(timer);
+	uv_unref(timer);
 }
 
+void timer_cb2 (uv_timer_t* timer, int status) {
+	printf("2\n", status);
+	uv_timer_stop(timer);
+	uv_unref(timer);
+}
 
-int main() {
-  int r = uv_thread_create(&thread, thread_cb, NULL);
-  assert(r == 0);
+int main () {
+	loop = uv_default_loop();
 
-  /* pause execution of this thread until the spawned thread has had
-   * time to finish execution. */
-  uv_thread_join(&thread);
+	uv_timer_t timer1;
+	uv_timer_init(loop, &timer1);
+	uv_timer_start(&timer1, (uv_timer_cb) &timer_cb1, 1000, 1000);
 
-  return 0;
+	uv_timer_t timer2;
+	uv_timer_init(loop, &timer2);
+	uv_timer_start(&timer2, (uv_timer_cb) &timer_cb2, 500, 1000);
+
+	uv_run(loop, UV_RUN_DEFAULT);
+
+	return 0;
 }
